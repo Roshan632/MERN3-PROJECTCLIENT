15ced411de5f1703eb438a60124532c5de67b2f8
Roshan Yadav <rosh45644@gmail.com>
2026-06-30 15:22:52 +0545

    Added single page Order

---
+++ src/pages/my-orders-details/types.ts
@@ -0,0 +1,62 @@
+export type OrderStatus =
+    | "preparation"
+    | "ontheway"
+    | "delivered"
+    | "pending"
+    | "cancelled";
+
+export const OrderStatus = {
+    Preparation: "preparation" as OrderStatus,
+    Ontheway: "ontheway" as OrderStatus,
+    Delivered: "delivered" as OrderStatus,
+    Pending: "pending" as OrderStatus,
+    Cancelled: "cancelled" as OrderStatus,
+} as const;
+
+export type PaymentMethod = "khalti" | "esewa" | "cod";
+
+export const PaymentMethod = {
+    Khalti: "khalti" as PaymentMethod,
+    Esewa: "esewa" as PaymentMethod,
+    COD: "cod" as PaymentMethod,
+} as const;
+
+export type PaymentStatus = "paid" | "unpaid";
+
+export const PaymentStatus = {
+    Paid: "paid" as PaymentStatus,
+    Unpaid: "unpaid" as PaymentStatus,
+} as const;
+
+export interface IOrderDetail {
+    id: string,
+    quantity: number,
+    createdAt: string,
+    
+    orderId: string,
+    productId: string,
+    Order: {
+        orderStatus: OrderStatus,
+        AddressLine: string,
+        City: string,
+        State: string,
+        totalAmount: number,
+        phoneNumber: string,
+        firstName : string, 
+        lastName : string, 
+        Payment: {
+            paymentMethod: PaymentMethod,
+            paymentStatus: PaymentStatus
+        }
+    },
+    Product: {
+        productImageUrl: string,
+        productName: string,
+        productPrice: number,
+        Category: {
+       
+            categoryName: string,
+      
+        }
+    }
+}
