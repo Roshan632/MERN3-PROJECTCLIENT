b4fc74d9c34f3f30287fdf3cd95482cd9f11a3b0
Roshan Yadav <rosh45644@gmail.com>
2026-07-02 15:41:07 +0545

    Product Admin table

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ import Categories from './pages/admin/categories/Categories'
 import AdminLayout from './pages/admin/AdminLayout'
 import AdminStats from './pages/admin/stats/AdminStats'
 import User from './pages/admin/users/Users'
-
+import AdminProduct from './pages/admin/products/Product'
 
 function App() {
   
@@ -40,6 +40,7 @@ function App() {
       <Route path='/admin' element={<AdminStats />} />
     <Route path='/admin/categories' element={<Categories />} />
      <Route path='/admin/users' element={<User />} />
+      <Route path='/admin/products' element={<AdminProduct />} />
      </Routes>
      
      </BrowserRouter>
